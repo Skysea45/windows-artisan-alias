22f10f73a4312da272c5fc4c1cc661179c89fb2d
skysea45 <skyseagundogdu45@gmail.com>
2024-05-18 17:07:05 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,87 @@
+
+# Windows "php artisan" Alias Creator
+
+This PowerShell script allows you to create a convenient alias for the `php artisan` command on Windows. The script automates the process of creating a batch file for the alias, adding the directory to the system PATH, and ensuring there are no conflicts with existing aliases. Additionally, it handles the cleanup of empty directories and their removal from the PATH if necessary.
+
+## Features
+
+- Creates a batch file alias for the `php artisan` command.
+- Adds the alias directory to the system PATH.
+- Checks for conflicts with existing aliases and handles them appropriately.
+- While creating an alias, if a php artisan alias already exists, it can clean up the old php artisan alias and delete its directory along with the PATH entry if there are no other files in the directory.
+
+## Prerequisites
+
+- Windows operating system.
+- Administrator privileges to modify the system PATH.
+
+## Security
+
+To ensure the integrity of the script, you can verify the SHA-256 hash of the downloaded script file. The SHA-256 hash for this script is:
+
+
+```
+SHA-256: 732285049061B3191E8678727798AD15EBAEFFCD82A9119DEF7CBB6AA8A33A2C
+```
+
+To verify the hash on your system, run the following PowerShell command:
+
+```powershell
+Get-FileHash path	windows-artisan-alias.ps1 -Algorithm SHA256 | Format-List
+```
+
+Replace `path windows-artisan-alias` with the actual path where you saved the script.
+
+## Usage
+
+1. **Download and Execute Script:**
+    - You can download and run the script directly from GitHub using the following PowerShell command:
+    ```powershell
+    $scriptUrl = "https://raw.githubusercontent.com/skysea45/windows-artisan-alias/main/windows-artisan-alias.ps1"
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString($scriptUrl)
+    ```
+
+2. **Starting the Script:**
+    - When the script starts, it will check for administrator privileges. If not running as an administrator, it will prompt for the necessary permissions.
+
+3. **Options Menu:**
+    - The script will present you with two options:
+        1. Start the process.
+        2. Exit.
+    - Select `1` to proceed with creating the alias or `2` to exit the script.
+
+4. **Directory Selection:**
+    - The default directory for storing the alias batch file is `C:\aliases` (it automatically detects your system disk even its not labeled as C ).
+    - You will be asked if you want to change the default directory. Press `n` to keep the default or `y` to specify a custom directory.
+
+5. **Alias Name Selection:**
+    - The default alias name is `pia`.
+    - You will be asked if you want to change the default alias name. Press `n` to keep the default or `y` to specify a custom alias name.
+
+6. **Conflict Check and Removal:**
+    - The script will check if there is any existing `php artisan` alias in the system PATH.
+    - If a conflict is found, you will be prompted to remove the existing alias and use the new one. Press `y` to proceed with the removal or `n` to cancel the alias creation process.
+
+7. **Alias Creation:**
+    - The script will create the alias batch file in the specified directory.
+    - If the directory does not exist, it will be created.
+    - The directory will be added to the system PATH if not already present.
+
+8. **Cleanup:**
+    - If an alias is removed and the directory becomes empty, the directory and its PATH entry will also be removed to keep the system clean.
+
+9. **Completion:**
+    - A message will be displayed indicating that the alias was successfully created and added to the PATH.
+    - The script will close automatically after a brief pause.
+
+##
+**Note: I have prepared this script based on my own testing and use cases. However, I am not responsible for any issues or damages that may arise from using this script.**
+##
+
+## Contributing
+
+Feel free to contribute to this project by submitting issues or pull requests on GitHub. Your feedback and contributions are greatly appreciated!
+
+## License
+
+This project is licensed under the MIT License.
---
+++ windows-artisan-alias.ps1
@@ -0,0 +1,227 @@
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+function Create-AliasFile {
+    param (
+        [string]$alias,
+        [string]$directory
+    )
+
+    # Create the new alias file
+    $aliasFile = "$directory\$alias.bat"
+    Set-Content -Path $aliasFile -Value "@echo off`nphp artisan %*"
+    Write-Output "Alias '$alias' created successfully."
+    return $true
+}
+
+function Add-ToPath {
+    param (
+        [string]$directory
+    )
+    $existingPath = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
+    if ($existingPath -notlike "*$directory*") {
+        $newPath = "$existingPath;$directory"
+        [System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::Machine)
+        Write-Output "Directory '$directory' added to PATH."
+    }
+}
+
+function Remove-FromPath {
+    param (
+        [string]$directory
+    )
+    $existingPath = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
+    if ($existingPath -like "*$directory*") {
+        $newPath = ($existingPath -split ';') -ne $directory -join ';'
+        [System.Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::Machine)
+        Write-Output "Directory '$directory' removed from PATH."
+    }
+}
+
+function Remove-PhpArtisanAlias {
+    param (
+        [string]$alias,
+        [string]$directory
+    )
+
+    $aliasFile = "$directory\$alias.bat"
+    if (Test-Path $aliasFile) {
+        Remove-Item -Path $aliasFile -Force
+        Write-Output "Existing alias file '$aliasFile' has been removed."
+
+        # Check if the directory is empty after removing the alias file
+        $remainingFiles = Get-ChildItem -Path $directory
+        if ($remainingFiles.Count -eq 0) {
+            Remove-Item -Path $directory -Force
+            Write-Output "Directory '$directory' is empty and has been removed."
+            Remove-FromPath -directory $directory
+        }
+    }
+}
+
+function Check-PhpArtisanAlias {
+    param (
+        [ref]$conflictPath,
+        [ref]$aliasName
+    )
+
+    $existingPath = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
+    $paths = $existingPath.Split(';')
+    foreach ($path in $paths) {
+        $aliasFiles = Get-ChildItem -Path $path -Filter "*.bat" -ErrorAction SilentlyContinue
+        foreach ($file in $aliasFiles) {
+            $content = Get-Content -Path $file.FullName
+            if ($content -match "php artisan") {
+                $conflictPath.Value = $path
+                $aliasName.Value = $file.BaseName
+                return $true
+            }
+        }
+    }
+    return $false
+}
+
+function Get-ScriptHash {
+    param (
+        [string]$scriptPath
+    )
+    $hash = Get-FileHash -Path $scriptPath -Algorithm SHA256
+    return $hash.Hash
+}
+
+# Function to restart the script
+function Restart-Script {
+    Clear-Host
+    Write-Output "Restarting script..."
+    & $PSCommandPath
+    exit
+}
+
+# Get the system drive
+$systemDrive = [System.IO.Path]::GetPathRoot([System.Environment]::SystemDirectory)
+
+# Get the script hash
+$scriptPath = $PSCommandPath
+$scriptHash = Get-ScriptHash -scriptPath $scriptPath
+
+# Main logic
+while ($true) {
+    Clear-Host
+    # Check for admin rights
+    if (-not (Test-Admin)) {
+        Write-Output "This script requires administrative privileges."
+        Write-Output "The script will request admin rights in 3 seconds..."
+        Start-Sleep -Seconds 3
+        Start-Process powershell "-File `"$PSCommandPath`"" -Verb RunAs
+        exit
+    }
+
+    Clear-Host
+    # Display initial options
+    Write-Output "Please select an option:"
+    Write-Output "1. Start the process"
+    Write-Output "2. Exit"
+    Write-Output ""
+    Write-Output "Script SHA-256: $scriptHash"
+    Write-Output "Developer: skysea45"
+    $initialChoice = Read-Host "Enter your choice (1/2)"
+    if ($initialChoice -eq "2" -or $initialChoice -eq "0") {
+        Clear-Host
+        Write-Output "Operation cancelled by user."
+        Write-Output "The script will close in 2 seconds..."
+        Start-Sleep -Seconds 2
+        exit
+    } elseif ($initialChoice -ne "1") {
+        Clear-Host
+        Write-Output "Invalid choice. Restarting script..."
+        Restart-Script
+    }
+
+    Clear-Host
+    # Default values
+    $defaultDirectory = "$systemDrive\aliases"
+    $defaultAlias = "pia"
+    $directory = $null
+    $alias = $null
+
+    # Inform user about the default directory
+    Write-Output "The alias will be added to the default directory: $defaultDirectory"
+    Write-Output "Press 0 to return to the main menu."
+    $response = Read-Host "Would you like to change the directory? (y/n) [n]"
+    if ($response -eq "0") {
+        continue
+    } elseif ($response -eq "y") {
+        Clear-Host
+        $customDirectory = Read-Host "Enter the custom directory"
+        if ($customDirectory -eq "0") {
+            continue
+        }
+        $directory = $customDirectory
+    } else {
+        $directory = $defaultDirectory
+    }
+
+    Clear-Host
+    # Inform user about the default alias
+    Write-Output "The default alias is '$defaultAlias'"
+    Write-Output "Press 0 to return to the main menu."
+    $response = Read-Host "Would you like to change the alias? (y/n) [n]"
+    if ($response -eq "0") {
+        continue
+    } elseif ($response -eq "y") {
+        Clear-Host
+        $alias = Read-Host "Enter the custom alias"
+        if ($alias -eq "0") {
+            continue
+        }
+    } else {
+        $alias = $defaultAlias
+    }
+
+    Clear-Host
+
+    # Check for existing php artisan alias in PATH
+    $conflictPath = ""
+    $aliasName = ""
+    if (Check-PhpArtisanAlias -conflictPath ([ref]$conflictPath) -aliasName ([ref]$aliasName)) {
+        $response = Read-Host "An alias for 'php artisan' already exists as '$aliasName' at '$conflictPath'. Do you want to remove it and use the new alias from the new directory? (y/n) [n]"
+        if ($response -eq "y") {
+            Remove-PhpArtisanAlias -alias $aliasName -directory $conflictPath
+        } else {
+            Write-Output "Alias creation cancelled due to conflict. Returning to the alias naming step."
+            continue
+        }
+    }
+
+    # Ensure the directory exists but do not create it yet
+    if (-not (Test-Path $directory)) {
+        $directoryExists = $false
+    } else {
+        $directoryExists = $true
+    }
+
+    # Remove existing php artisan alias in the target directory
+    Remove-PhpArtisanAlias -alias $alias -directory $directory
+
+    # Create the directory if it does not exist
+    if (-not $directoryExists) {
+        New-Item -Path $directory -ItemType Directory
+        Write-Output "Directory '$directory' created successfully."
+    }
+
+    # Create alias file
+    if (-not (Create-AliasFile -alias $alias -directory $directory)) {
+        continue
+    }
+
+    Clear-Host
+    # Add directory to PATH
+    Add-ToPath -directory $directory
+
+    Write-Output "Alias '$alias' created and added to PATH successfully."
+    Write-Output "The script will close in 5 seconds..."
+    Start-Sleep -Seconds 5
+    exit
+}
